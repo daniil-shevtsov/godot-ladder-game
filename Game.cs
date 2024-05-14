de85db348d7d6b4aef1ca307c424d90c69ae410c
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-05-15 00:54:26 +0400

    x rotation works

--- Game.cs
+++ Game.cs
@@ -6,12 +6,14 @@ public partial class Game : Node3D
 	private Player player;
 	private Ladder ladder;
 	private Marker3D cameraPivot;
+	private Camera3D camera;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player = GetNode<Player>("Player");
 		ladder = GetNode<Ladder>("Ladder");
 		cameraPivot = GetNode<Marker3D>("CameraPivot");
+		camera = (Camera3D)FindChild("Camera3D");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,4 +21,21 @@ public partial class Game : Node3D
 	{
 		cameraPivot.GlobalPosition = player.GlobalPosition;
 	}
+
+	// accumulators
+	private float _rotationX = 0f;
+	private float _rotationY = 0f;
+	public override void _Input(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion eventMouseMotion)
+		{
+			player.Rotation = new Vector3(
+				player.Rotation.X,
+				player.Rotation.Y - eventMouseMotion.Relative.X / sensitivity,
+				player.Rotation.Z
+			);
+		}
+	}
+
+	private float sensitivity = 1000f;
 }
