fac131377ecea3eb22c6ac7bad727b26d38c1210
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-05-15 00:24:22 +0400

    Make camera move with player

--- Game.cs
+++ Game.cs
@@ -4,14 +4,17 @@ using System;
 public partial class Game : Node3D
 {
 	private Player player;
+	private Marker3D cameraPivot;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player = GetNode<Player>("Player");
+		cameraPivot = GetNode<Marker3D>("CameraPivot");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		cameraPivot.GlobalPosition = player.GlobalPosition;
 	}
 }
