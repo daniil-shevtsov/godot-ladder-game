62b99fc069d48f7aabae7e47e8bbacf334467838
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-05-16 11:24:27 +0400

    grab and drop work

--- Game.cs
+++ Game.cs
@@ -4,14 +4,19 @@ using System;
 public partial class Game : Node3D
 {
 	private Player player;
+	private StaticBody3D hand;
+	private HingeJoint3D ladderBottomHinge;
 	private Ladder ladder;
 	private CollisionShape3D ladderEnd;
 	private Marker3D cameraPivot;
 	private Camera3D camera;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		player = (Player)FindChild("Player");
+		hand = (StaticBody3D)FindChild("Hand");
+		ladderBottomHinge = (HingeJoint3D)FindChild("LadderBottomHinge");
 		ladder = GetNode<Ladder>("Ladder");
 		ladderEnd = (CollisionShape3D)FindChild("LadderEnd");
 		cameraPivot = (Marker3D)FindChild("CameraPivot");
@@ -26,6 +31,12 @@ public partial class Game : Node3D
 
 	public override void _PhysicsProcess(double delta)
 	{
+
+		if (Input.IsActionJustPressed("grab"))
+		{
+			OnGrabPressed();
+		}
+
 		if (Input.IsActionJustPressed("push_ladder_up"))
 		{
 			ladder.ApplyForce(new Vector3(0f, ladderPushForce, 0f), ladderEnd.GlobalPosition);
@@ -54,6 +65,24 @@ public partial class Game : Node3D
 				cameraPivot.Rotation.Z
 			);
 		}
+	}
+
+	private void OnGrabPressed()
+	{
+		GD.Print($"Grab pressed, current NodeA: {ladderBottomHinge.NodeA}");
+		var grabbedPath = ladderBottomHinge.GetPathTo(hand);
+		GD.Print($"Path to: {grabbedPath}");
+		if (ladderBottomHinge.NodeA == grabbedPath)
+		{
+			GD.Print($"Drop");
+			ladderBottomHinge.NodeA = null;
+		}
+		else
+		{
+			GD.Print($"Take");
+			ladderBottomHinge.NodeA = grabbedPath;
+		}
+		GD.Print($"New nodeA: {ladderBottomHinge.NodeA}");
 	}
 
 	private float sensitivity = 1000f;
